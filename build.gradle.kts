213745320ab0412912b372d5f591f4d2534d7a81
Andrea Severi <andrea.severi.dev@gmail.com>
2023-10-04 19:13:47 +0200

    correct plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
 plugins {
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.protobuf) apply false
-    alias(libs.plugins.junit5) apply false
-
-    alias(libs.plugins.gradle.versions)
-    alias(libs.plugins.version.catalog.update)
-    alias(libs.plugins.android.library) apply false
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
-
-apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
