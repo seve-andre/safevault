f87b58ea843f5140102cfbb1f0726ad2e513805d
Andrea Severi <andrea.severi.dev@gmail.com>
2023-10-15 16:56:21 +0200

    create helper function to retrieve string in instrumented tests

--- core/testing/src/main/kotlin/com/mitch/safevault/core/testing/util/Matchers.kt
+++ core/testing/src/main/kotlin/com/mitch/safevault/core/testing/util/Matchers.kt
@@ -1,2 +1,13 @@
 package com.mitch.safevault.core.testing.util
 
+import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.getStringById(
+    @StringRes id: Int,
+    vararg formatArgs: Any
+): String {
+    return this.activity.resources.getString(id, formatArgs)
+}
