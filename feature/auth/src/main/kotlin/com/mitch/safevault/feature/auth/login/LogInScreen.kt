bf939afef8e7d31f969858e29b55cfe4723b986a
Andrea Severi <andrea.severi.dev@gmail.com>
2023-10-24 13:40:04 +0200

    adopt eager validation for email

--- feature/auth/src/main/kotlin/com/mitch/safevault/feature/auth/login/LogInScreen.kt
+++ feature/auth/src/main/kotlin/com/mitch/safevault/feature/auth/login/LogInScreen.kt
@@ -4,14 +4,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mitch.safevault.core.designsystem.theme.padding
 import com.mitch.safevault.core.ui.component.email.EmailState
@@ -19,6 +28,7 @@ import com.mitch.safevault.core.ui.component.email.EmailTextField
 import com.mitch.safevault.core.ui.component.password.PasswordState
 import com.mitch.safevault.core.ui.component.password.PasswordTextField
 import com.mitch.safevault.feature.auth.R
+import kotlinx.coroutines.delay
 import com.mitch.safevault.core.util.R as utilR
 
 @Composable
@@ -34,6 +44,7 @@ internal fun LogInRoute(
     )
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal fun LogInScreen(
     emailState: EmailState,
@@ -41,6 +52,13 @@ internal fun LogInScreen(
     onLogInSubmitted: (String, String) -> Unit,
     onNavigateToSignUp: () -> Unit
 ) {
+    val (emailFocusRequester, passwordFocusRequester) = remember { FocusRequester.createRefs() }
+
+    LaunchedEffect(Unit) {
+        delay(1_000)
+        emailFocusRequester.requestFocus()
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -48,17 +66,32 @@ internal fun LogInScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmailTextField(emailState = emailState)
-        PasswordTextField(passwordState = passwordState)
+        EmailTextField(
+            emailState = emailState,
+            modifier = Modifier.focusRequester(emailFocusRequester)
+        )
+        PasswordTextField(
+            passwordState = passwordState,
+            modifier = Modifier
+                .focusRequester(passwordFocusRequester)
+                .onFocusChanged { if (it.isFocused) emailState.shouldStartValidation = true },
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onLogInSubmitted(emailState.email, passwordState.password)
+                }
+            )
+        )
         Button(
             onClick = {
                 onLogInSubmitted(emailState.email, passwordState.password)
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
         ) {
             Text(text = stringResource(id = utilR.string.log_in))
         }
-        TextButton(onClick = { /*TODO*/ }) {
+        TextButton(onClick = onNavigateToSignUp) {
             Text(text = stringResource(id = R.string.sign_up_now))
         }
     }
